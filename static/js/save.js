ce3c81e2481611e45a28ff4ce4e26f81f87fa3b8
Jack Singleton <git@jacksingleton.com>
2015-02-10 15:42:10 -0800

    refresh markdown directly, and do so automatically after typing stops

--- static/js/save.js
+++ static/js/save.js
@@ -1,7 +1,5 @@
 function save() {
   var editor = ace.edit("editor");
   $.post("/slides.md", editor.getValue());
-
-  var iframe = document.getElementById('slides-frame');
-  iframe.contentWindow.location.reload(true);
+  $('#slides-frame')[0].contentWindow.postMessage('save', window.location.origin)
 }
