8656b3b06e0ad53b8cc52a7e8d7279065269b5d0
Jack Singleton <git@jacksingleton.com>
2015-02-11 15:44:10 -0800

    use the Reveal postMessage api

--- static/js/save.js
+++ static/js/save.js
@@ -1,5 +1,9 @@
 function save() {
   var editor = ace.edit("editor");
+
   $.ajax("/slides.md", {type: 'put', data: editor.getValue()});
-  $('#slides-frame')[0].contentWindow.postMessage('save', window.location.origin)
+
+  $('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
+    method: 'reloadMarkdown'
+  }), window.location.origin);
 }
