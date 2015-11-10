0c42d0ec1692985f1785781ba634e59a84e100dc
Jack Singleton <git@jacksingleton.com>
2015-11-09 21:43:04 -0800

    pull js and css out of html files

---
+++ static/js/index.js
@@ -0,0 +1,56 @@
+$(function() {
+
+  function slideSeparatorLines(text) {
+    var lines = text.split('\n');
+
+    var separatorLineNumbers = [];
+
+    for (i = 0; i < lines.length; i++) {
+      var line = lines[i];
+      if (line === '---') {
+        separatorLineNumbers.push(i);
+      }
+    }
+
+    return separatorLineNumbers;
+  }
+
+  function currentCursorSlide(cursorLine) {
+    var text = ace.edit("editor").getValue();
+    var separatorPositions = slideSeparatorLines(text);
+    var slideNumber = separatorPositions.length;
+    separatorPositions.every(function(pos, num) {
+      if (pos >= cursorLine) {
+        slideNumber = num;
+        return false;
+      }
+      return true;
+    });
+    return slideNumber;
+  }
+
+
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/chrome");
+  editor.getSession().setMode("ace/mode/markdown");
+  editor.getSession().setUseWrapMode(true);
+  editor.setShowPrintMargin(true);
+
+  $.get('/slides.md', function(data) {
+    editor.setValue(data, -1);
+  });
+
+  $('#editor').keyup($.debounce(save, 300));
+
+  ace.edit('editor').getSession().selection.on('changeCursor', function(e) {
+    var cursorRow = ace.edit('editor').getCursorPosition().row;
+    var currentSlide = currentCursorSlide(cursorRow);
+    $('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
+      method: 'slide',
+      args: [currentSlide]
+    }), window.location.origin);
+  });
+});
+
+
+
---
+++ static/js/slides.js
@@ -0,0 +1,73 @@
+function isPreview() {
+  return !!window.location.search.match(/preview/gi);
+}
+
+function initializeReveal() {
+  // Full list of configuration options available at:
+  // https://github.com/hakimel/reveal.js#configuration
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+    transitionSpeed: isPreview() ? 'fast' : 'default',
+    embedded: isPreview() ? true : false,
+
+      // Optional reveal.js plugins
+    dependencies: [
+    { src: '/static/revealjs/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    { src: '/static/revealjs/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: '/static/revealjs/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); }, callback: function() { externalLinksInNewWindow(); } },
+      { src: '/static/revealjs/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+      { src: '/static/revealjs/plugin/zoom-js/zoom.js', async: true },
+        { src: '/static/revealjs/plugin/notes/notes.js', async: true }
+    ]
+  });
+}
+
+function highlightAnyCodeBlocks() {
+  $(document).ready(function() {
+    $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+  });
+}
+
+function insertMarkdownReference() {
+  var markdownReference = $('<section/>', {
+    'data-markdown': "../slides.md",
+    'data-separator': "^-( *)-( *)-( *-*)*",
+    'data-separator-notes': "^Note:",
+    'data-charset': "utf-8"
+  });
+
+  $('.slides').html(markdownReference);
+}
+
+function scrollToCurrentSlide() {
+  var i = Reveal.getIndices();
+  Reveal.slide(i.h, i.v, i.f);
+}
+
+function reloadMarkdown() {
+  insertMarkdownReference();
+  RevealMarkdown.initialize();
+  highlightAnyCodeBlocks();
+  scrollToCurrentSlide();
+}
+
+function externalLinksInNewWindow() {
+  $(document.links).filter(function() {
+    return this.hostname != window.location.hostname;
+  }).attr('target', '_blank');
+}
+
+insertMarkdownReference();
+initializeReveal();
+
+// Monkey patch Reveal so we can reload markdown through an
+// inter window message (using the reveal rpc api)
+// (yes, reveal has an rpc api!)
+// see save.js
+Reveal.reloadMarkdown = reloadMarkdown;
