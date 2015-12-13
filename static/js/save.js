c4f6b26b3ac41d5f0c03d8999e98a35751428a4f
Jack Singleton <git@jacksingleton.com>
2015-12-12 17:53:44 -0800

    only reload after successful save
    let's see if this results in more reliable previews

--- static/js/save.js
+++ static/js/save.js
@@ -1,13 +1,19 @@
 $(function() {
-  window.save = function() {
-    var editor = ace.edit("editor");
-
-    $.ajax("/slides.md", {type: 'put', data: editor.getValue()});
-
+  function reloadMarkdown() {
     $('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
       method: 'reloadMarkdown'
     }), window.location.origin);
   }
+
+  window.save = function() {
+    var editor = ace.edit("editor");
+
+    $.ajax("/slides.md", {
+      type: 'put',
+      data: editor.getValue(),
+      success: reloadMarkdown
+    });
+  };
 
   $('#editor').keyup($.debounce(window.save, 300));
 });
