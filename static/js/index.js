f599a3e12c08a6989b81c242beab591b083c600b
Jack Singleton <git@jacksingleton.com>
2016-01-16 17:17:42 -0800

    add plain text mode

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,8 @@
+function plainTextMode() {
+  var queryString = document.location.search;
+  return queryString.indexOf('plain-text-mode') > -1;
+}
+
 $(function() {
 
   function slideSeparatorLines(text) {
@@ -29,26 +34,48 @@ $(function() {
     return slideNumber;
   }
 
+  function setupAceEditor() {
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    editor.getSession().setMode("ace/mode/markdown");
+    editor.getSession().setUseWrapMode(true);
+    editor.setShowPrintMargin(true);
 
-  var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/chrome");
-  editor.getSession().setMode("ace/mode/markdown");
-  editor.getSession().setUseWrapMode(true);
-  editor.setShowPrintMargin(true);
+    $.get('/slides.md', function(data) {
+      editor.setValue(data, -1);
+    });
 
-  $.get('/slides.md', function(data) {
-    editor.setValue(data, -1);
-  });
+    ace.edit('editor').getSession().selection.on('changeCursor', function(e) {
+      var cursorRow = ace.edit('editor').getCursorPosition().row;
+      var currentSlide = currentCursorSlide(cursorRow);
+      $('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
+        method: 'slide',
+        args: [currentSlide]
+      }), window.location.origin);
+    });
+  }
 
-  ace.edit('editor').getSession().selection.on('changeCursor', function(e) {
-    var cursorRow = ace.edit('editor').getCursorPosition().row;
-    var currentSlide = currentCursorSlide(cursorRow);
-    $('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
-      method: 'slide',
-      args: [currentSlide]
-    }), window.location.origin);
-  });
+  function setupTextArea() {
+    var textArea = $('<textarea>');
+
+    $('#editor').html(textArea);
+
+    $.get('/slides.md', function(data) {
+      textArea[0].value = data;
+    });
+  }
+
+  function updateTextModeLink(text, href) {
+    var link = $('#plain-text-link')
+    link.text(text);
+    link.attr('href', href);
+  }
+
+  if (plainTextMode()) {
+    setupTextArea();
+    updateTextModeLink('Rich-Text Mode', '/');
+  } else {
+    setupAceEditor();
+    updateTextModeLink('Plain-Text Mode', '/?plain-text-mode');
+  }
 });
-
-
-
