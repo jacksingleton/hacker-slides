efe6b9bad50cda310cbb6b087235a3880625269d
Thierry Pasquier <thierry@ks3279941.kimsufi.com>
2016-04-13 23:56:06 +0200

    Fix indentation

--- static/js/index.js
+++ static/js/index.js
@@ -5,80 +5,80 @@ function plainTextMode() {
 
 $(function() {
 
-		function linesToSlides(text) {
+	function linesToSlides(text) {
 		var lines = text.split('\n');
 		var slideIndex = 0 ;
 		var subSlideIndex = 0 ;
 		var lineToSlide = new Array;
 
 		for (i = 0; i < lines.length; i++) {
-		var line = lines[i];
-		var result = new Array;	
-		result.slide = slideIndex ;
-		result.subslide = subSlideIndex ;
-		if (line === '---') {
-		slideIndex++ ;
-		subSlideIndex = 0;
-		} else if (line === '--') {
-		subSlideIndex++ ;
-		}
-		lineToSlide.push(result) ;
+			var line = lines[i];
+			var result = new Array;	
+			result.slide = slideIndex ;
+			result.subslide = subSlideIndex ;
+			if (line === '---') {
+				slideIndex++ ;
+				subSlideIndex = 0;
+			} else if (line === '--') {
+				subSlideIndex++ ;
+			}
+			lineToSlide.push(result) ;
 		}
 
 		return lineToSlide;
-		}
+	}
 
-		function currentCursorSlide(cursorLine) {
-			var text = ace.edit("editor").getValue();
-			var position = linesToSlides(text);
-			var slideNumber = [] ;
-			slideNumber.h = position[cursorLine].slide;
-			slideNumber.v = position[cursorLine].subslide;
-			return slideNumber;
-		}
+	function currentCursorSlide(cursorLine) {
+		var text = ace.edit("editor").getValue();
+		var position = linesToSlides(text);
+		var slideNumber = [] ;
+		slideNumber.h = position[cursorLine].slide;
+		slideNumber.v = position[cursorLine].subslide;
+		return slideNumber;
+	}
 
-		function setupAceEditor() {
-			var editor = ace.edit("editor");
-			editor.setTheme("ace/theme/chrome");
-			editor.getSession().setMode("ace/mode/markdown");
-			editor.getSession().setUseWrapMode(true);
-			editor.setShowPrintMargin(true);
+	function setupAceEditor() {
+		var editor = ace.edit("editor");
+		editor.setTheme("ace/theme/chrome");
+		editor.getSession().setMode("ace/mode/markdown");
+		editor.getSession().setUseWrapMode(true);
+		editor.setShowPrintMargin(true);
 
-			$.get('/slides.md', function(data) {
-					editor.setValue(data, -1);
-					});
+		$.get('/slides.md', function(data) {
+			editor.setValue(data, -1);
+		});
 
-			ace.edit('editor').getSession().selection.on('changeCursor', function(e) {
-					var cursorRow = ace.edit('editor').getCursorPosition().row;
-					var currentSlide = currentCursorSlide(cursorRow);
-					$('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
-method: 'slide',
-args: [currentSlide.h, currentSlide.v]
-}), window.location.origin);
-					});
-}
+		ace.edit('editor').getSession().selection.on('changeCursor', function(e) {
+			var cursorRow = ace.edit('editor').getCursorPosition().row;
+			var currentSlide = currentCursorSlide(cursorRow);
+			$('#slides-frame')[0].contentWindow.postMessage(JSON.stringify({
+				method: 'slide',
+				args: [currentSlide.h, currentSlide.v]
+			}), window.location.origin);
+		});
+	}
 
-function setupTextArea() {
-	var textArea = $('<textarea>');
+	function setupTextArea() {
+		var textArea = $('<textarea>');
 
-	$('#editor').html(textArea);
+		$('#editor').html(textArea);
 
-	$.get('/slides.md', function(data) {
+		$.get('/slides.md', function(data) {
 			textArea[0].value = data;
-			});
-}
+		});
+	}
 
-function updateTextModeLink(text, href) {
-	var link = $('#plain-text-link')
-		link.text(text);
-	link.attr('href', href);
-}
+	function updateTextModeLink(text, href) {
+		var link = $('#plain-text-link')
+			link.text(text);
+		link.attr('href', href);
+	}
 
-if (plainTextMode()) {
-	setupTextArea();
-	updateTextModeLink('Rich-Text Mode', '/');
-} else {
-	setupAceEditor();
-	updateTextModeLink('Plain-Text Mode', '/?plain-text-mode');
-}
+	if (plainTextMode()) {
+		setupTextArea();
+		updateTextModeLink('Rich-Text Mode', '/');
+	} else {
+		setupAceEditor();
+		updateTextModeLink('Plain-Text Mode', '/?plain-text-mode');
+	}
 });
